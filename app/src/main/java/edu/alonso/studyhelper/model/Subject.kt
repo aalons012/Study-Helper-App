d91744c7ea8ac92fa44b403cda01b6c058b32e8f
Andy Alonso <Andyalonso281@gmail.com>
2025-10-21 00:31:10 -0400

    added code to model/Subject.kt

--- app/src/main/java/edu/alonso/studyhelper/model/Subject.kt
+++ app/src/main/java/edu/alonso/studyhelper/model/Subject.kt
@@ -1,4 +1,11 @@
 package edu.alonso.studyhelper.model
 
-class Subject {
-}
+// Data class representing a Subject
+data class Subject (
+    // Unique identifier for the subject, with a default value of 0
+    var id: Long = 0,
+    // The text or name/description of the subject
+    var text: String,
+    // The last update time of the subject, defaults to the current time
+    var updateTime: Long = System.currentTimeMillis()
+)
