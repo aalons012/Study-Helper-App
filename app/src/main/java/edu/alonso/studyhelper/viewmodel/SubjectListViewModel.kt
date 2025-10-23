932d5d659561c4662913e609044b4b1c9b9432e6
Andy Alonso <Andyalonso281@gmail.com>
2025-10-22 20:21:35 -0400

    modded code to SubjectListViewModel

--- app/src/main/java/edu/alonso/studyhelper/viewmodel/SubjectListViewModel.kt
+++ app/src/main/java/edu/alonso/studyhelper/viewmodel/SubjectListViewModel.kt
@@ -1,4 +1,14 @@
 package edu.alonso.studyhelper.viewmodel
 
-class SubjectListViewModel {
+import android.app.Application
+import edu.alonso.studyhelper.model.Subject
+import edu.alonso.studyhelper.repo.StudyRepository
+
+class SubjectListViewModel(application: Application) {
+
+    private val studyRepo = StudyRepository.getInstance(application.applicationContext)
+
+    fun getSubjects(): List<Subject> = studyRepo.getSubjects()
+
+    fun addSubject(subject: Subject) = studyRepo.addSubject(subject)
 }
