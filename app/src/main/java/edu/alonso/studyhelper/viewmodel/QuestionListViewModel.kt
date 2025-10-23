11cb43bedfc38c2fe78f082868d6ad12f8a8e48d
Andy Alonso <Andyalonso281@gmail.com>
2025-10-22 20:20:38 -0400

    modded code to QuestionListViewModel

--- app/src/main/java/edu/alonso/studyhelper/viewmodel/QuestionListViewModel.kt
+++ app/src/main/java/edu/alonso/studyhelper/viewmodel/QuestionListViewModel.kt
@@ -1,4 +1,12 @@
 package edu.alonso.studyhelper.viewmodel
 
-class QuestionListViewModel {
+import android.app.Application
+import edu.alonso.studyhelper.model.Question
+import edu.alonso.studyhelper.repo.StudyRepository
+
+class QuestionListViewModel (application: Application) {
+
+    private val studyRepo = StudyRepository.getInstance(application.applicationContext)
+
+    fun getQuestions(subjectId: Long): List<Question> = studyRepo.getQuestions(subjectId)
 }
