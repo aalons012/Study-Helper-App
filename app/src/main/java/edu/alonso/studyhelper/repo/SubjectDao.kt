b88e95c768c32cca75b552ce6df0b289398e156f
Andy Alonso <Andyalonso281@gmail.com>
2025-10-24 01:54:14 -0400

    adding code

--- app/src/main/java/edu/alonso/studyhelper/repo/SubjectDao.kt
+++ app/src/main/java/edu/alonso/studyhelper/repo/SubjectDao.kt
@@ -1,4 +1,22 @@
 package edu.alonso.studyhelper.repo
 
-class SubjectDao {
+import androidx.room.*
+import edu.alonso.studyhelper.model.Subject
+
+@Dao
+interface SubjectDao {
+    @Query("SELECT * FROM Subject WHERE id = :id")
+    fun getSubject(id: Long): Subject?
+
+    @Query("SELECT * FROM Subject ORDER BY text COLLATE NOCASE")
+    fun getSubjects(): List<Subject>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addSubject(subject: Subject): Long
+
+    @Update
+    fun updateSubject(subject: Subject)
+
+    @Delete
+    fun deleteSubject(subject: Subject)
 }
