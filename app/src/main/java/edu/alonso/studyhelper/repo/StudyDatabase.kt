7fc7c2bfc9cb00b85be45f32bd83cd653e5460f5
Andy Alonso <Andyalonso281@gmail.com>
2025-10-24 11:53:57 -0400

    added code to StudyDatabase

--- app/src/main/java/edu/alonso/studyhelper/repo/StudyDatabase.kt
+++ app/src/main/java/edu/alonso/studyhelper/repo/StudyDatabase.kt
@@ -1,4 +1,13 @@
 package edu.alonso.studyhelper.repo
 
-class StudyDatabase {
+import androidx.room.RoomDatabase
+import androidx.room.Database
+import edu.alonso.studyhelper.model.Question
+import edu.alonso.studyhelper.model.Subject
+
+@Database(entities = [Question::class, Subject::class], version = 1)
+abstract class StudyDatabase : RoomDatabase() {
+
+    abstract fun questionDao(): QuestionDao
+    abstract fun subjectDao(): SubjectDao
 }
